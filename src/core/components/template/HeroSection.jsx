fd2bdb42fab9674e9441df8ff0c07c5b1f73ef40
iAldrich23xX <mapasalejo@gmail.com>
2026-02-02 22:12:42 -0500

    fix(Hero): add image in background & major design, need more changes and approve

--- src/core/components/template/HeroSection.jsx
+++ src/core/components/template/HeroSection.jsx
@@ -1,12 +1,18 @@
 import DotGrid from '@core/components/organisms/DotGrid/DotGrid.jsx';
 import mark from "@core/assets/tire-mark.png";
+import background from "@core/assets/width_1088.webp"
 import React from 'react';
 import Logo from '@core/components/atoms/Logo.jsx';
 
 export default function HeroSection() {
     return (
-        <div className="w-full h-200 relative">
-            <div className="absolute inset-0 -z-10 overflow-hidden">
+        <section className="w-full h-250 relative">
+            <div
+                className="absolute inset-0 -z-20 bg-cover bg-center bg-no-repeat bg-fixed"
+                style={{ backgroundImage: `url(${background})` }}
+            />
+
+            <div className="absolute inset-0 overflow-hidden">
                 <div className="tire-img-wrap">
                     <img
                         src={mark}
@@ -20,7 +26,7 @@ export default function HeroSection() {
             <DotGrid
                 dotSize={8}
                 gap={10}
-                baseColor="bg-accent"
+                baseColor="bg-accent/30"
                 activeColor="bg-primary"
                 proximity={120}
                 shockRadius={250}
@@ -28,10 +34,10 @@ export default function HeroSection() {
                 resistance={750}
                 returnDuration={1.5}
             >
-                <h1 className="text-5xl md:text-7xl font-extrabold mb-8 tracking-tight leading-tight text-center">
-                    Finanzas reales
-                    <span className="block text-primary-soft drop-shadow-[0_0_20px_rgba(157,105,168,0.8)]">
-                        en cada kilómetro
+                <h1 className="text-5xl md:text-7xl text-foreground-invert font-extrabold mb-8 tracking-tight leading-tight text-center drop-shadow-[0_0_20px_rgba(106,18,106,1)]">
+                    FINANZAS REALES
+                    <span className="block text-primary-soft">
+                        EN CADA KILÓMETRO
                     </span>
                 </h1>
             </DotGrid>
@@ -41,6 +47,18 @@ export default function HeroSection() {
                     <Logo size="hero" variant="hero" showText={false} />
                 </div>
             </div>
-        </div>
+
+            <div
+                className="
+                    pointer-events-none
+                    absolute bottom-0 left-0 right-0
+                    h-100
+                    z-20
+                    bg-gradient-to-b
+                    from-transparent
+                    to-background-end
+                "
+            />
+        </section>
     );
 }
